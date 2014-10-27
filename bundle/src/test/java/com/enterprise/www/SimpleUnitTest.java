94824826532753cff9a6d63d4e1f0e0989033ff5
Nikunj Jariwala <njariwala@roundarchisobar.com>
2014-10-27 17:56:30 -0400

    NJ: Maven multi module project added for AEM.

---
+++ bundle/src/test/java/com/enterprise/www/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.enterprise.www;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
